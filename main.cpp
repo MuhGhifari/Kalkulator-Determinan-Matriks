ec27dac60bac69cdb40a39e37758fb3598f89853
MuhGhifari <muhammadghifari37@gmail.com>
2024-06-17 21:40:30 +0700

    ubah output

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-vector<vector<double>> getMinor(const vector<vector<double>> &matriks, int row, int col) {
+vector<vector<double>> ambilMatriksMinor(const vector<vector<double>> &matriks, int row, int col) {
 	vector<vector<double>> minor;
 	int n = matriks.size();
 	for (int i = 0; i < n; i++) {
@@ -17,20 +17,21 @@ vector<vector<double>> getMinor(const vector<vector<double>> &matriks, int row, 
 		}
 		minor.push_back(minorRow);
 	}
+	
 	return minor;
 }
 
-double determinan(const vector<vector<double>>& matriks) {
+double hitungDeterminan(const vector<vector<double>>& matriks) {
 	int n = matriks.size();
 	if (n == 1) return matriks[0][0];
 	if (n == 2) return matriks[0][0] * matriks[1][1] - matriks[0][1] * matriks[1][0];
 	
-	double det = 0;
+	double determinan = 0;
 	for (int col = 0; col < n; col++) {
-		vector<vector<double>> minor = getMinor(matriks, 0, col);
-		det += (col % 2 == 0 ? 1 : -1) * matriks[0][col] * determinan(minor);
+		vector<vector<double>> minor = ambilMatriksMinor(matriks, 0, col);
+		determinan += (col % 2 == 0 ? 1 : -1) * matriks[0][col] * hitungDeterminan(minor);
 	}
-	return det;
+	return determinan;
 }
 
 int main() {
@@ -45,7 +46,6 @@ int main() {
 		string isiBaris;
 		stringstream ss(str);
 		vector<double> input;
-		bool inputValid = true;
 
 		while (getline(ss, isiBaris, ',')) {
 			input.push_back(stod(isiBaris));
@@ -55,9 +55,18 @@ int main() {
 	fileInput.close();
 
 
-	double det = determinan(matriks);
-	system("clear"); // utk windows ganti jadi system("cls")
-	cout << "Determinannya adalah : " << det << endl;
+	double det = hitungDeterminan(matriks);
+	system("clear");
+	cout << "Matriks : " << endl;
+	for (int i = 0; i < matriks.size(); i++) {
+		cout << "| ";
+		for (int j = 0; j < matriks[i].size(); j++) {
+			cout << matriks[i][j] << " ";
+		}
+
+		cout << "|" << endl;
+	}
+	cout << "\nDeterminannya adalah : " << det << endl;
 	
 	return 0;
 }
